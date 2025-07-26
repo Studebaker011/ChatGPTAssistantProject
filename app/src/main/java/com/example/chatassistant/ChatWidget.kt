1c85990e727aaa4615c9e77082946a923f7628f0
deprezjeremy00@gnail.com <deprezjeremy00@gmail.com>
2025-07-26 15:53:34 -0400

    Initial ChatGPT Assistant project

---
+++ app/src/main/java/com/example/chatassistant/ChatWidget.kt
@@ -0,0 +1,13 @@
+
+package com.example.chatassistant
+
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.appwidget.AppWidgetManager
+import android.content.Intent
+
+class ChatWidget : AppWidgetProvider() {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // TODO: update widget views
+    }
+}
